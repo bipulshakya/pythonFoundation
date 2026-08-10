0b93bf5a249ad9fb3b67ccc36e1bb7c47fd95c2f
bipulshakya <154542673+bipulshakya@users.noreply.github.com>
2026-08-10 07:44:06 +0545

    basic-python

---
+++ calc_grade.py
@@ -0,0 +1,14 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return 'A'
+    elif marks >= 80:
+        return 'B'
+    elif marks >= 70:
+        return 'C'
+    elif marks >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+grade = int(input("Enter your marks: "))
+print(grade)
---
+++ func.py
@@ -0,0 +1,8 @@
+def function_sum(a, b):
+    return a + b
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+sum = function_sum(a, b)
+print(sum)
